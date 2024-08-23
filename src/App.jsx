ab33ca339e18e27a5f5738459724401d1fcc77e9
pyhumph <humphrey.david.dev@gmail.com>
2024-08-23 19:57:52 +0300

    Add navigation bar section with responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="overflow-x-hidden text-neutral-950 antialiased selection:bg-cyan-300 selection:bg-cyan-900">
+      <div className="flixed top-0 -z-10 h-full w-full"></div>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div>
+
+      <div className="container mx-auto px-8">
+        <Navbar />
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from "../assets/logo.svg";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaSquareTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+
+function Navbar() {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 30" src={logo} alt="" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <FaLinkedin />
+        <FaGithub />
+        <FaInstagram />
+        <FaSquareTwitter />
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
