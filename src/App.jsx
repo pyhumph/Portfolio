8e645ef2860c85ba4708a64b2509b901e3de9038
pyhumph <humphrey.david.dev@gmail.com>
2024-08-24 12:02:37 +0300

    Render new component About section in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="overflow-x-hidden text-neutral-950 antialiased selection:bg-cyan-300 selection:bg-cyan-900">
-      <div className="flixed top-0 -z-10 h-full w-full"></div>
-      <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div>
+      <div className="fixed top-0 -z-10 h-full w-full">
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div>
+      </div>
 
       <div className="container mx-auto px-8">
         <Navbar />
         <Hero />
+        <About />
       </div>
     </div>
   );
