bd451b76a6b2466af9097e7d9a803a16d617cb97
pyhumph <humphrey.david.dev@gmail.com>
2024-08-24 18:30:04 +0300

    Format Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,9 @@ function Contact() {
       <div className="text-center tracking-tighter">
         <p className="my-4">{CONTACT.address}</p>
         <p className="my-4">{CONTACT.phoneNo}</p>
-        <a href="#" className="border-b">{CONTACT.email}</a>
+        <a href="#" className="border-b">
+          {CONTACT.email}
+        </a>
       </div>
     </div>
   );
