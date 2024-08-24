7382c2566c6a518d419786750628040606454ea9
pyhumph <humphrey.david.dev@gmail.com>
2024-08-24 22:01:33 +0300

    update the website theme

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="overflow-x-hidden text-neutral-950 antialiased selection:bg-cyan-300 selection:bg-cyan-900">
       <div className="fixed top-0 -z-10 h-full w-full">
-        <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div>
+        <div className="absolute top-0 z-[-2] h-screen w-screen rotate-180 transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]"></div>
       </div>
 
       <div className="container mx-auto px-8">
