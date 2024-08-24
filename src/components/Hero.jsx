8dfdc837fe83f4a2e5fdf6cdfcd9d3c66e8ac61f
pyhumph <humphrey.david.dev@gmail.com>
2024-08-24 12:04:57 +0300

    Update video tag in hero.js: removed autoplay, muted, playsInline, and loop attribute

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ function Hero() {
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
             <div className="flex justify-center">
-                <video autoPlay loop muted playsInline src={profile}></video>
+                <video src={profile}></video>
             </div>
         </div>
       </div>
