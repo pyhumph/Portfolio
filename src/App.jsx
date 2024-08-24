431308ab4af38a9c7ba6e9f78d380bf1f8823d32
pyhumph <humphrey.david.dev@gmail.com>
2024-08-24 17:27:57 +0300

    Render new component Contact.jsx in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from "./components/About";
 import Technologies from "./components/Technologies";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Technologies />
         <Experience />
         <Projects />
+        <Contact />
       </div>
     </div>
   );
